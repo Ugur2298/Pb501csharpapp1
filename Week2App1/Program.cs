02a8d1e871d94e349a5b4d068d4a78690105b73b
Ughur Gasimov <ughurvg@code.edu.az>
2024-01-21 17:38:17 +0400

    c# app1

---
+++ Week2App1/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Week2App1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            float firstNumber;
+            float secondNumber;
+
+            Console.WriteLine("Please enter first number");
+            firstNumber=float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter second number");
+            secondNumber = float.Parse(Console.ReadLine());
+
+            float resultSum= firstNumber+secondNumber;
+            Console.WriteLine("Addition result: "+ resultSum);
+
+            float resultMinus= firstNumber-secondNumber;
+            Console.WriteLine("Minus result: "+ resultMinus);
+
+            float resultMuliply= firstNumber*secondNumber;
+            Console.WriteLine("Multiply result: "+ resultMuliply);
+
+            float resultDivision= firstNumber/secondNumber;
+            Console.WriteLine("Division result: "+ resultDivision);
+
+            Console.WriteLine("Hello World");
+
+
+
+
+        }
+    }
+}
