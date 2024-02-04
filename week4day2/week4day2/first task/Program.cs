f8b3b91fc1839c89c351dd64afc531f14ddfb769
Ughur Gasimov <ughurvg@code.edu.az>
2024-02-04 19:28:56 +0400

    week4day2 tasks added

---
+++ week4day2/week4day2/first task/Program.cs
@@ -0,0 +1,91 @@
+﻿using System.Text;
+
+namespace tasks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Zehmet olmasa birinci ededi daxil edin: ");
+            //int number1=int.Parse(Console.ReadLine());
+            //Console.WriteLine("Zehmet olmasa ikinci ededi daxil edin: ");
+            //int number2=int.Parse(Console.ReadLine());
+
+            //int callResult= CalculateSumfTwoNumbers(number1, number2);
+            //Console.WriteLine("Iki eded arasindaki ededlerin cemi"+ callResult);
+
+            //int callResultOfAverage = CalculateAverageOfTwoNumbers(number1, number2);
+            //Console.WriteLine("Iki eded arasindaki ededlerin cemi" + callResultOfAverage);
+
+            //courseRegister();
+
+
+
+        }
+
+        //public static int CalculateSumfTwoNumbers(int num1, int num2) 
+        //{
+        //    int sum= 0;
+        //    for (int i=num1; i < num2; i++)
+        //    {
+        //        sum += i;
+        //    }
+        //    return sum;
+        //}
+
+        //public static int CalculateAverageOfTwoNumbers(int num1, int num2)
+        //{
+        //    int sumOfAverage = 0;
+        //    int totalSum= 0;
+        //    int count= 0;
+
+        //    for (int i = num1; i < num2; i++)
+        //    {
+        //        totalSum += i;
+        //        count++;
+
+
+        //    }
+        //    sumOfAverage= totalSum/count;
+        //    return sumOfAverage;
+
+        //}
+
+
+        //public static void courseRegister()
+        //{
+        //start:
+        //    Console.Write("Telebenin kurs sirasi: ");
+        //    int id = int.Parse(Console.ReadLine());
+        //    Console.Write("Telebenin yasi: ");
+        //    int age = int.Parse(Console.ReadLine());
+        //    Console.Write("Telebenin texniki ingilisce bali: ");
+        //    int englishGrade = int.Parse(Console.ReadLine());
+        //    if (age >= 18 && englishGrade >= 50)
+        //    {
+        //        Console.WriteLine("Telebe qeyde alindi");
+        //    }
+        //    else { Console.WriteLine("Telebe qeyde alinmadi"); }
+
+        //    StringBuilder student = new StringBuilder();
+        //    student.Append($"Kurs sirasi: {id} ---").Append($"Yasi: {age} ---").Append($"Texniki ingilisce bali: {englishGrade}");
+        //    List<StringBuilder> studentList = new List<StringBuilder>();
+        //    studentList.Add(student);
+
+        //    foreach (StringBuilder person in studentList)
+        //    {
+        //        Console.WriteLine($"Telebelerin siyahisi: \n{person.ToString()}");
+        //    }
+
+
+        //    Console.WriteLine("Basqa telebe qeyde alinsin mi? (beli: b, xeyr: x)");
+        //    char answer = char.Parse(Console.ReadLine());
+        //    if (answer == 'b')
+        //    {
+        //        goto start;
+        //    }
+
+        //}
+
+        }
+}
